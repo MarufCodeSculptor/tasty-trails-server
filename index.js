86a3a8c28406b297f152bc533b6d9def2c021963
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-07-13 17:10:59 +0600

    deleted cart route added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Root Access");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.6mzg5rv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -57,6 +57,12 @@ async function run() {
       const result = await cartCollections.insertOne(data);
       res.send(result);
     });
+    app.delete('/carts/:id',async(req,res)=>{
+      const itemId = req.params.id;
+      const query = { _id : new ObjectId(itemId)};
+      const result = await cartCollections.deleteOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
